8233bf4b9585b26843f81bf47494903d6718f5d8
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-22 21:00:29 +0500

    Setup react-redux linters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const App = () => {
-  return <div>Hello React Redux Capstone!</div>;
-};
+const App = () => <div>Hello React Redux Capstone!</div>;
 
 export default App;
