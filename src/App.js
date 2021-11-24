4b78583f14c6289180317d3761d3d9370fc7099a
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-24 15:05:22 +0500

    Add react-routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Details from './pages/Details';
 
-const App = () => <div>Hello React Redux Capstone!</div>;
+const App = () => (
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" exact>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/details">Details</NavLink>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/details">
+        <Details />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
