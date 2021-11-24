10d7ac02d042631f767fb697226b1364b494a7e1
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-24 15:04:27 +0500

    Create home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
-  return <div>Home Page</div>;
+  const uk = ['england-and-wales', 'northern-ireland', 'scotland', 'uk'];
+  const [englandData, setEnglandData] = useState([]);
+  const [irelandData, setIrelandData] = useState([]);
+  const [scotlandData, setScotlandData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // const handleAPI = async () => {
+  //   const response = await axios.get('https://www.gov.uk/bank-holidays.json');
+  //   const data = await response.data;
+  //   setEnglandData(data['england-and-wales'].events);
+  //   setIrelandData(data['northern-ireland'].events);
+  //   setScotlandData(data.scotland.events);
+  // };
+
+  const response = [...englandData, ...irelandData, ...scotlandData];
+  console.log('Response: ', response);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search ..."
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <hr />
+      <ol>
+        {uk
+          .filter((value) => {
+            if (value.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return value;
+            }
+          })
+          .map((resp) => (
+            <li key={resp}>{resp}</li>
+          ))}
+      </ol>
+    </div>
+  );
 };
 
 export default Home;
