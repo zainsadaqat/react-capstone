8b94f749d7f99bd045e766c3a0767ef4b0b450e5
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-25 09:15:32 +0500

    Update actions

---
+++ src/store/actions/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+// Action Type
+export const ENGLAND_DATA = 'ReactReduxCapstone/UK_BANK_HOLIDAYS/ENGLAND_DATA';
+export const IRELAND_DATA = 'ReactReduxCapstone/UK_BANK_HOLIDAYS/IRELAND_DATA';
+export const SCOTLAND_DATA =
+  'ReactReduxCapstone/UK_BANK_HOLIDAYS/SCOTLAND_DATA';
+
+export const initialState = { england: [], ireland: [], scotland: [] };
+
+// Action Creators
+
+export const fetchEnglandData = () => async (dispatch) => {
+  const response = await axios.get('https://www.gov.uk/bank-holidays.json');
+  const data = await response.data;
+
+  dispatch({ type: ENGLAND_DATA, payload: data['england-and-wales'].events });
+};
+
+export const fetchIrelandData = () => async (dispatch) => {
+  const response = await axios.get('https://www.gov.uk/bank-holidays.json');
+  const data = await response.data;
+
+  dispatch({ type: IRELAND_DATA, payload: data['northern-ireland'].events });
+};
+
+export const fetchScotlandData = () => async (dispatch) => {
+  const response = await axios.get('https://www.gov.uk/bank-holidays.json');
+  const data = await response.data;
+
+  dispatch({ type: SCOTLAND_DATA, payload: data.scotland.events });
+};
