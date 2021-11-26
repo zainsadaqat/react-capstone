c5f8c6d28eaa69f71852693f98558d8bdbc516db
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-26 21:54:00 +0500

    Create routes for application

--- src/App.js
+++ src/App.js
@@ -1,36 +1,64 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from 'react-router-dom';
-import Home from './pages/Home';
-import Details from './pages/Details';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Header from './components/header/Header';
+import Details from './pages/details/Details';
+import Error404 from './pages/Error404';
 
-const App = () => (
-  <Router>
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/details">Details</NavLink>
-        </li>
-      </ul>
-    </nav>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/details">
-        <Details />
-      </Route>
-    </Switch>
-  </Router>
-);
+const App = () => {
+  const [total, setTotal] = useState([]);
+
+  const england = useSelector((state) => state.Reducer.england);
+  const ireland = useSelector((state) => state.Reducer.ireland);
+  const scotland = useSelector((state) => state.Reducer.scotland);
+
+  return (
+    <Router>
+      <nav>
+        <Header />
+      </nav>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              total={total}
+              setTotal={setTotal}
+              england={england}
+              ireland={ireland}
+              scotland={scotland}
+            />
+          }
+        />
+        <Route
+          path="/details"
+          element={
+            <Details
+              total={total}
+              setTotal={setTotal}
+              england={england}
+              ireland={ireland}
+              scotland={scotland}
+            />
+          }
+        />
+        <Route
+          path="/details/:country"
+          element={
+            <Details
+              total={total}
+              setTotal={setTotal}
+              england={england}
+              ireland={ireland}
+              scotland={scotland}
+            />
+          }
+        />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
