3cfa560eae0c25023ce4eaca90d807b6f9f2c6f5
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-27 08:31:53 +0500

    Write test case for components

---
+++ src/__tests__/components.react.test.js
@@ -0,0 +1,27 @@
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+
+import store from '../store/index';
+import EnglandData from '../components/data/england/EnglandData';
+import IrelandData from '../components/data/ireland/IrelandData';
+
+describe('Testing react-redux-capstone project Components', () => {
+  test('Testing EnglandData Component', () => {
+    const englandData = renderer.create(
+      <Provider store={store}>
+        <EnglandData />
+      </Provider>
+    );
+    const tree = englandData.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('Testing IrelandData Component', () => {
+    const irelandData = renderer.create(
+      <Provider store={store}>
+        <IrelandData />
+      </Provider>
+    );
+    const tree = irelandData.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
